49fa0044c8df0c1ff45d3bcd40ccce9795b78088
Martin Jesper Low Madsen <mj@blackbird.online>
2020-06-18 10:55:30 +0200

    Support extending tsconfigs

--- src/asset.ts
+++ src/asset.ts
@@ -1,5 +1,5 @@
 import TypeScriptAsset = require('parcel-bundler/src/assets/TypeScriptAsset.js');
-import JSAsset = require('parcel-bundler/src/assets/JSAsset.js');
+import config = require('parcel-bundler/src/utils/config.js');
 import * as path from 'path';
 
 const IMPORT_RE = /\b(?:import(?:.|[\n\r])+?|export(?:.|[\n\r])+?|require.+?)\(?['"](.*)['"]\)?/g;
@@ -22,10 +22,7 @@ class TypeScriptModuledResolveAsset extends TypeScriptAsset {
     key: string
   ) {
     const relativePath = path
-      .relative(
-        path.resolve(this.name, '..'),
-        `${this.options.rootDir}/${newPath}`
-      )
+      .relative(path.dirname(this.name), newPath)
       .replace(/\\/g, '/');
 
     const targetFile =
@@ -36,13 +33,51 @@ class TypeScriptModuledResolveAsset extends TypeScriptAsset {
     return `./${relativePath}${targetFile}`.replace(/\/{2,}/g, '/');
   }
 
-  public async fixImports(code: string) {
-    const tsconfig = await super.getConfig(['tsconfig.json']); // Overwrite default if config is found
+  private async getConfig(filenames: string[], { path: searchPath = path.dirname(this.name) }: { path?: string } = {}) {
+    const [primaryFile] = filenames;
+    const fileName = path.basename(primaryFile);
 
-    const paths: { [key: string]: string[] } = tsconfig.compilerOptions.paths;
+    const configPath = await config.resolve(searchPath, [fileName]);
+    const configFile = path.basename(configPath);
+    const configDirectory = path.dirname(configPath);
+
+    const { compilerOptions, extends: _extends, ...cfg } = await super.getConfig([configFile], { path: configPath });
+
+    const {
+      compilerOptions: extendingCompilerOptions,
+      ...extendingConfig
+    } = await ((isExtending) => {
+      if (isExtending) {
+        const fileName = path.basename(isExtending);
+        return this.getConfig([fileName], { path: path.resolve(configDirectory, isExtending) });
+      } else {
+        return { compilerOptions: {} };
+      }
+    })(_extends);
+
+    let baseUrl = compilerOptions.baseUrl || extendingCompilerOptions.baseUrl || '.';
+    baseUrl = path.isAbsolute(baseUrl) ? baseUrl : path.resolve(configDirectory, baseUrl);
+
+    return {
+      compilerOptions: {
+        ...extendingCompilerOptions,
+        ...compilerOptions,
+        baseUrl,
+      },
+      ...extendingConfig,
+      ...cfg,
+    };
+  }
+
+  public async fixImports(code: string) {
+    const tsconfig = await this.getConfig(['tsconfig.json']);
+
+    const { baseUrl, paths }: { baseUrl: string, paths: { [key: string]: string[] } } = tsconfig.compilerOptions;
+
     if (typeof paths === 'undefined' || paths === null) {
       return;
     }
+
     const pairs = Object.keys(paths).map((key) => {
       const newKey = key.replace('/*', '/');
       return { [newKey]: paths[key][0].replace('/*', '') };
@@ -59,7 +94,7 @@ class TypeScriptModuledResolveAsset extends TypeScriptAsset {
         ) {
           substr = substr.replace(
             includePath,
-            this.getRelativeIncludePath(newPaths[key], includePath, key)
+            this.getRelativeIncludePath(path.join(baseUrl, newPaths[key]), includePath, key)
           );
 
           return substr;
